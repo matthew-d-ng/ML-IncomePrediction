812ef8944b5c82fd01dbe96ceb8d5a3cbb24a3e5
Matthew Ng <ngm1@tcd.ie>
2019-10-13 20:28:29 +0100

    now scales data, fetching about 80k rmse

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ from sklearn import linear_model as lm
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas
 
@@ -22,7 +23,7 @@ no_str_columns = [
 ]
 
 ohe_columns = [
-    "Gender", "Country", "Profession", "University Degree"
+    "Country", "Profession", "University Degree", "Gender", "Hair Color"
 ]
 
 input_columns = [
@@ -36,8 +37,7 @@ target_columns = [
 ]
 
 def drop_columns(dataframe):
-    """ Remove unnecessary input columns before inserting into model
-    """
+    """ Remove unnecessary input columns before inserting into model """
     return dataframe.drop(
         [
             "Hair Color",
@@ -47,38 +47,12 @@ def drop_columns(dataframe):
         axis=1
     )
 
-def bad_model(labelled_data, unlabelled_data):
-    """ Replaces NaN values with -1, 
-        ignores non-numeric fields,
-        does not modify data in any other way.
-    """
-    # removing NaN fields
-    clean_labelled = labelled_data.dropna() # so a -1 won't skew the model
-    clean_unlabelled = unlabelled_data[no_str_columns] # income field is NaN
-    clean_unlabelled = clean_unlabelled.fillna(-1) # don't want to remove any
+def lin_model(labelled_data, unlabelled_data):
+    """ Parameters: training dataframe, unknown dataframe
+        Returns: results dataframe
 
-    # specifying relevant columns
-    training_data = clean_labelled[no_str_columns]
-    target_data = clean_labelled[["Income in EUR"]]
-    test_data = clean_unlabelled[no_str_columns]
-
-    # building model
-    lin_reg = lm.LinearRegression()
-    lin_reg.fit(training_data, target_data)
-
-    # getting results and formatting them to DataFrame
-    values = lin_reg.predict(test_data)
-    results = pandas.DataFrame({
-        "Instance": clean_unlabelled["Instance"].values,
-        "Income": values.flatten()
-    })
-    return results
-
-def better_model(labelled_data, unlabelled_data):
-    """ Replaces NaN values with column mean,
-        categorises non-numeric fields with OneHotEncoder,
-        80/20 splits data to help verify model,
-        use LassoCV model
+        Replaces NaN with ffill, one hot encodes non-numeric fields, 
+        80/20 splits data to help verify model, uses LassoCV model
     """
     print("cleaning data...")
     clean_labelled = labelled_data.dropna()
@@ -86,9 +60,9 @@ def better_model(labelled_data, unlabelled_data):
     clean_unlabelled = clean_unlabelled.fillna(method="ffill")
     clean_unlabelled = clean_unlabelled.fillna("None")
 
-    # clean input data
-    clean_labelled = drop_columns(clean_labelled)
-    clean_unlabelled = drop_columns(clean_unlabelled)
+    # remove some columns
+    # clean_labelled = drop_columns(clean_labelled)
+    # clean_unlabelled = drop_columns(clean_unlabelled)
 
     print("one hot encoding data...")
     # One hot encoding
@@ -104,18 +78,20 @@ def better_model(labelled_data, unlabelled_data):
 
     print("splitting data into train and test...")
     # 80/20 split
-    train, test = train_test_split(clean_labelled, test_size=0.2)
+    split = split_data(clean_labelled)
+    train_data, train_target, test_data, test_target = split
 
-    training_data = train.drop(["Income in EUR"], axis=1).drop(["Instance"], axis=1)
-    target_data = train[target_columns]
-
-    test_data = test.drop(["Income in EUR"], axis=1).drop(["Instance"], axis=1)
-    test_target = test[target_columns]
+    print("scaling values...")
+    # scaling values
+    scaler = StandardScaler()
+    train_data = scaler.fit_transform(train_data)
+    test_data = scaler.transform(test_data)
+    unknown_data = scaler.transform(unknown_data)
 
     print("fitting model...")
     # fit model
-    lasso = lm.LassoCV(cv=3)
-    lasso.fit(training_data, target_data)
+    lasso = lm.LassoCV(cv=5)
+    lasso.fit(train_data, train_target)
 
     print("analysing test results...")
     # validate test
@@ -130,21 +106,25 @@ def better_model(labelled_data, unlabelled_data):
         "Instance": clean_unlabelled["Instance"].values,
         "Income": values.flatten()
     })
+    print("Finished.")
     return results
 
 def encode_training(ohe, dataframe):
+    """ Perform One Hot Encoding on labelled dataframe"""
     x = constrain_col_vals(dataframe)
     arr = ohe.fit_transform(x[ohe_columns])
     x = merge_encoding(ohe, x, arr)
     return x
 
 def encode_testing(ohe, dataframe):
+    """ Perform One Hot Encoding on unlabelled dataframe """
     x = constrain_col_vals(dataframe)
     arr = ohe.transform(x[ohe_columns])
     x = merge_encoding(ohe, x, arr)
     return x
 
 def merge_encoding(ohe, dataframe, arr):
+    """ Insert encoded values back into original dataframe """
     col_names = ohe.get_feature_names(ohe_columns)
     encoded = pandas.DataFrame(arr, columns = col_names)
     dataframe = dataframe.drop(ohe_columns, axis=1).reset_index(drop=True)
@@ -152,23 +132,39 @@ def merge_encoding(ohe, dataframe, arr):
     return dataframe
 
 def constrain_col_vals(dataframe):
+    """ Standardise input variants of necessary categories """
     x = dataframe
 
-    mapping = {"male": 0, "female": 1, "other": 2}
-    x["Gender"] = x["Gender"].map(mapping)
+    values = {"male": 0, "female": 1, "other": 2}
+    x["Gender"] = x["Gender"].map(values)
     x["Gender"] = x["Gender"].fillna(2)
 
-    mapping = {"No": 0, "Bachelor": 1, "Master": 2, "PhD": 3}
-    x["University Degree"] = x["University Degree"].map(mapping)
+    values = {"No": 0, "Bachelor": 1, "Master": 2, "PhD": 3}
+    x["University Degree"] = x["University Degree"].map(values)
     x["University Degree"] = x["University Degree"].fillna(0)
 
+    values = {"Black": 0, "Brown": 1, "Red": 2, "Blond": 3}
+    x["Hair Color"] = x["Hair Color"].map(values)
+    x["Hair Color"] = x["Hair Color"].fillna(4)
+
     return x
+
+def split_data(dataframe):
+    """ Splits data into training and test, also splits input from target"""
+    train, test = train_test_split(dataframe, test_size=0.2)
+
+    train_data = train.drop(["Income in EUR"], axis=1).drop(["Instance"], axis=1)
+    train_target = train[target_columns]
+
+    test_data = test.drop(["Income in EUR"], axis=1).drop(["Instance"], axis=1)
+    test_target = test[target_columns]
+
+    return (train_data, train_target, test_data, test_target)
 
 
 if __name__ == "__main__":
     labelled_data = pandas.read_csv(with_labels)
     unlabelled_data = pandas.read_csv(without_labels)
 
-    # testing(labelled_data)
-    results = better_model(labelled_data, unlabelled_data)
+    results = lin_model(labelled_data, unlabelled_data)
     results.to_csv(results_file, index=False)
